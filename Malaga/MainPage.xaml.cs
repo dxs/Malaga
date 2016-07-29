b4d6559187a822afdb3392d759572d77b1d2bf88
Sven <glaglasven@live.com>
2016-07-29 23:14:30 +0200

    Added scroolviewver

--- Malaga/MainPage.xaml.cs
+++ Malaga/MainPage.xaml.cs
@@ -955,6 +955,20 @@ namespace Malaga
 				mainMap.TrafficFlowVisible = false;
 		}
 
+		private void OnScrollViewerViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+		{
+    		var verticalOffset = sv.VerticalOffset;
+    		var maxVerticalOffset = sv.ScrollableHeight; //sv.ExtentHeight - sv.ViewportHeight;
+
+    		if (maxVerticalOffset < 0 || verticalOffset == maxVerticalOffset)
+    		{
+        		// Scrolled to bottom
+			}
+    		else
+    		{
+        		// Not scrolled to bottom
+    		}
+		}
 		#region foursquare
 
 		/*
